3ef9f5b2d5e0f938af71d9fb2932e0d1cd0c3d8b
jdeluna06 <jdeluna0672@gmail.com>
2024-08-28 17:12:08 -0700

    updated weather and hostory bonus

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -4,28 +4,25 @@ const router = Router();
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
-
-// TODO: POST Request with city name to retrieve weather data
-// TODO: GET weather data from city name
-  // TODO: save city to search history
-
 router.post('/', (req: Request, res: Response) => {
   try {
     const cityName = req.body.cityName;
     WeatherService.getWeatherForCity(cityName).then((data: any) => {
       HistoryService.addCity(data[0].city);
       res.json(data);
-    }).catch((error) => {
-    res.status(500).json(error);
-    })
+    });
+
+  } catch (error) {
+      res.status(500).json(error);
+  }
   });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {
+router.get('/history', (_req: Request, res: Response) => {
   HistoryService.getCities()
-  .then((cities) => {
+  .then((data) => {
     return res.json(data);
-}).catch((error) => {
+  }).catch((error) => {
   res.status(500).json(error);
   });
 });
@@ -34,12 +31,13 @@ router.get('/history', async (req: Request, res: Response) => {
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {
   if (!req.params.id) {
-    res.status(400).json({ message: "City ID is required" });
+    res.status(400).json({ error: "City ID is required" });
   }
    HistoryService.removeCity(req.params.id) .then(() => {
     res.json({ message: "City removed from history" });
-    
-  }
+  }).catch((error) => {
+    res.status(500).json(error);
+  });
 });
 
 export default router;
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -13,15 +13,33 @@ class HistoryService {
   }
 
   async getCities() {
-    return await this.read().then(cities) => {
-    let parsedCities: City[]
+    return await this.read().then(cities => {
+    let parsedCities: City[];
     try {
       parsedCities = [].concat(JSON.parse(cities));
     } catch (error) {
+      console.log(error);
+
       parsedCities = [];
     }
     return parsedCities;
+  })
   }
+  async addCity(city: string) {
+    if (!city) {
+      throw new Error("City name is required");
+    }
+    const newCity: City = {name: city, id: uuidv4() };
+    return await this.getCities().then((cities) => {
+      if (cities.find(index => index.name === city)) {
+        return cities;
+      }
+      return [...cities, newCity];
+    })
+    .then(updatedCities => this.write(updatedCities));
+    .then(() => newCity);
+    }
+      
   // TODO: Define a read method that reads from the searchHistory.json file
   // private async read() {}
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
@@ -31,7 +49,12 @@ class HistoryService {
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  async removeCity(id: string) {
+    return await this.getCities().then((cities) => {
+      const updatedCities = cities.filter(city => city.id !== id);
+      return this.write(updatedCities);
+    });
+  }
 }
 
 export default new HistoryService();
