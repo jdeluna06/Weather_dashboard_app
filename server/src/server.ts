22c1e6ac0fe6e6e5c3e7ef12d7ca7dd845a5e75c
jdeluna06 <jdeluna0672@gmail.com>
2024-08-28 17:56:21 -0700

    make corrections to the weatherService.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,6 @@ dotenv.config();
 import routes from './routes/index.js';
 
 
-// TODO: Serve static files of entire client dist folder
-
-// TODO: Implement middleware for parsing JSON and urlencoded form data
-
-// TODO: Implement middleware to connect the routes
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -21,3 +16,14 @@ app.use(routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+
+
+
+
+
+// TODO: Serve static files of entire client dist folder
+
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+
+// TODO: Implement middleware to connect the routes
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,11 +1,20 @@
-// TODO: Define a City class with name and id properties
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
+
+class City {
+  id: string;
+  name: string;
+
+  constructor(name: string, id: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   private async read() {
-    return await fs.readFile("db.jason", JSON.stringify(cities, null, 2));
+    return await fs.readFile("db.jason", "utf-8");
   }
 
   private async write(cities: City[]) {
@@ -14,41 +23,34 @@ class HistoryService {
 
   async getCities() {
     return await this.read().then(cities => {
-    let parsedCities: City[];
-    try {
-      parsedCities = [].concat(JSON.parse(cities));
-    } catch (error) {
-      console.log(error);
+      let parsedCities: City[]
+      try {
+        parsedCities = [].concat(JSON.parse(cities));
+      } catch (error) {
+        console.log(error);
 
-      parsedCities = [];
-    }
-    return parsedCities;
-  })
+
+        parsedCities = [];
+      }
+      return parsedCities;
+    })
   }
   async addCity(city: string) {
     if (!city) {
       throw new Error("City name is required");
     }
     const newCity: City = {name: city, id: uuidv4() };
-    return await this.getCities().then((cities) => {
+    return await this.getCities().then(cities => {
       if (cities.find(index => index.name === city)) {
         return cities;
       }
       return [...cities, newCity];
     })
-    .then(updatedCities => this.write(updatedCities));
+    .then(updatedCities => this.write(updatedCities))
     .then(() => newCity);
     }
       
-  // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
-  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
-  // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
-  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
+  
   async removeCity(id: string) {
     return await this.getCities().then((cities) => {
       const updatedCities = cities.filter(city => city.id !== id);
@@ -58,3 +60,15 @@ class HistoryService {
 }
 
 export default new HistoryService();
+
+
+
+// TODO: Define a read method that reads from the searchHistory.json file
+  // private async read() {}
+  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
+  // private async write(cities: City[]) {}
+  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+  // async getCities() {}
+  // TODO Define an addCity method that adds a city to the searchHistory.json file
+  // async addCity(city: string) {}
+  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
